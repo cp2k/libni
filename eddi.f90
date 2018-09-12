a9c159cd6fb44b781cc98d5bc830cce0c174eead
Edward Ditler <editler@student.ethz.ch>
2018-09-12 10:03:37 +0200

    two atom grid

--- eddi.f90
+++ eddi.f90
@@ -6,87 +6,111 @@ module eddi
    implicit none
    REAL(KIND=dp), PARAMETER :: pi = 3.14159265358979323846264338_dp ! Pi
 
-   public :: integration_oneatom
+   public :: integration_twoatom
 
    contains
-      subroutine integration_oneatom(nleb, nshell)
+      subroutine integration_twoatom(nleb, nshell, displacement, integral)
          implicit none
-         INTEGER, intent(in) :: nleb, nshell
-         INTEGER :: ileb
-         INTEGER :: ngrid = 0   ! number of points making up the grid
+         INTEGER, DIMENSION(2), intent(in) :: nleb, nshell
+         REAL(KIND=dp), DIMENSION(3), intent(in) :: displacement
+         INTEGER, DIMENSION(2) :: ileb
+         INTEGER :: ngrid
          TYPE(type_grid_point), DIMENSION(:), ALLOCATABLE :: thegrid
          REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: thefgrid
+         REAL(KIND=dp) :: integral
 
-         ileb = get_number_of_lebedev_grid(nleb)
-         ngrid = lebedev_grid(ileb)%n * nshell
+         ileb(1) = get_number_of_lebedev_grid(nleb(1))
+         ileb(2) = get_number_of_lebedev_grid(nleb(2))
+         ngrid = lebedev_grid(ileb(1))%n * nshell(1) + &
+                 lebedev_grid(ileb(2))%n * nshell(2)
 
-         print *, '!' // REPEAT('-', 79)
-         print *, '! Angular grid points: ', lebedev_grid(ileb)%n
-         print *, '! Radial grid points: ', nshell
+         print *, '!' // REPEAT('-', 78) // '!'
+         print *, '! Angular grid1 points: ', lebedev_grid(ileb(1))%n
+         print *, '! Radial grid1 points: ', nshell(1)
+         print *, '! Angular grid2 points: ', lebedev_grid(ileb(2))%n
+         print *, '! Radial grid2 points: ', nshell(2)
          print *, '! Total grid points:   ', ngrid
-         print *, '!' // REPEAT('-', 79)
+         print *, '!' // REPEAT('-', 78) // '!'
          allocate(thegrid(ngrid))
          allocate(thefgrid(ngrid))
 
-         call build_grid(ileb, nshell, thegrid)
-         call evaluate_atgrid(thegrid, thefgrid)
+         call build_grid(ileb, nshell, displacement, thegrid)
          ! call print_grid(thegrid, thefgrid)
-         print *, sum(thefgrid)
+         call evaluate_atgrid(thegrid, thefgrid)
+         integral = 4.0_dp*pi*sum(thefgrid)
 
          deallocate(thegrid)
          deallocate(thefgrid)
 
-      end subroutine integration_oneatom
+      end subroutine integration_twoatom
 
-      subroutine evaluate_atgrid(thegrid, thefgrid)
+      subroutine build_grid(ileb, nshell, displacement, thegrid)
          implicit none
          TYPE(type_grid_point), DIMENSION(:), ALLOCATABLE :: thegrid
-         REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: thefgrid
-         REAL(KIND=dp) :: norm
-         INTEGER :: i
-
-         do i=1,size(thefgrid)
-            norm = sqrt(sum((thegrid(i)%r)**2))
-            thefgrid(i) = thegrid(i)%weight * gaussian(norm)
-         enddo 
-      end subroutine evaluate_atgrid
-
-      subroutine build_grid(ileb, nshell, thegrid)
-         implicit none
-         TYPE(type_grid_point), DIMENSION(:), ALLOCATABLE :: thegrid
-         INTEGER, intent(in) :: ileb, nshell
+         INTEGER, DIMENSION(2), intent(in) :: ileb, nshell
+         REAL(KIND=dp), DIMENSION(3), intent(in) :: displacement
          INTEGER :: cnt, iterrad, iterang
-         REAL(KIND=dp) :: tradw, tangw, tsin, tcos, targ
+         REAL(KIND=dp) :: tradw, tangw, tsin, tcos, targ, tr
          REAL(KIND=dp) :: alpha
 
          if (.not. allocated(thegrid)) then
-            print *, 'Allocating the grid in build_grid.' !CP
-            allocate(thegrid(lebedev_grid(ileb)%n * nshell))
+            print *, 'The grid has not been allocated!' !CP
          end if
 
-         alpha = pi/REAL(nshell+1, dp)
          cnt = 0
-         do iterang=1, lebedev_grid(ileb)%n
-            tangw = lebedev_grid(ileb)%w(iterang)
-            do iterrad=1, nshell
-               cnt = cnt+1
-               ! targ = alpha*(2.0_dp*iterrad+1)/2.0_dp
-               ! tsin = sin(targ)
-               ! tcos = cos(targ)
 
-               ! tradw = 1 / sqrt(1-tcos**2)
+         alpha = pi/REAL(nshell(1)+1, dp)
+         do iterang=1, lebedev_grid(ileb(1))%n
+            tangw = lebedev_grid(ileb(1))%w(iterang)
+            do iterrad=1, nshell(1)
+               cnt = cnt+1
                targ = REAL(iterrad, dp)*alpha
                tcos = cos(targ)
+               tr = radial_mapping(tcos)
                tsin = alpha*sin(targ)**2
                tradw = tsin/sqrt(1.0_dp-tcos**2)
-               tradw = 2.0_dp*tradw*radial_mapping(tcos)**2/(1.0_dp-tcos)**2
+               tradw = 2.0_dp*tradw*tr**2/(1.0_dp-tcos)**2
 
-               thegrid(cnt)%r = radial_mapping(tcos)*lebedev_grid(ileb)%r(:, iterang)
+               thegrid(cnt)%r = tr*lebedev_grid(ileb(1))%r(:, iterang)
+               thegrid(cnt)%weight = tangw * tradw
+            enddo !iterrad
+         enddo !iterang
+
+         alpha = pi/REAL(nshell(2)+1, dp)
+         do iterang=1, lebedev_grid(ileb(2))%n
+            tangw = lebedev_grid(ileb(2))%w(iterang)
+            do iterrad=1, nshell(2)
+               cnt = cnt+1
+               targ = REAL(iterrad, dp)*alpha
+               tcos = cos(targ)
+               tr = radial_mapping(tcos)
+               tsin = alpha*sin(targ)**2
+               tradw = tsin/sqrt(1.0_dp-tcos**2)
+               tradw = 2.0_dp*tradw*tr**2/(1.0_dp-tcos)**2
+
+               thegrid(cnt)%r = tr*lebedev_grid(ileb(2))%r(:, iterang) +&
+                                 displacement
                thegrid(cnt)%weight = tangw * tradw
             enddo !iterrad
          enddo !iterang
          
       end subroutine build_grid
+
+      subroutine evaluate_atgrid(thegrid, thefgrid)
+         implicit none
+         TYPE(type_grid_point), DIMENSION(:), ALLOCATABLE :: thegrid
+         REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: thefgrid
+         REAL(KIND=dp) :: norm, f1, f2, partition
+         INTEGER :: i
+
+         partition = 0.5_dp
+         do i=1,size(thefgrid)
+            norm = sqrt(sum((thegrid(i)%r)**2))
+            f1 = gaussian(norm)
+            f2 = 1.0_dp
+            thefgrid(i) = thegrid(i)%weight * partition * f1 * f2
+         enddo
+      end subroutine evaluate_atgrid
 
       subroutine print_grid(thegrid, thefgrid)
          implicit none
@@ -124,6 +148,8 @@ module eddi
 
       end function gaussian
 
+!! SUBROUTINE GRAVEYARD
+
       ! Take input N and return an array of N values evenly spaced
       ! between [-lower, upper]
       ! function range(N, lower, upper) result(r)
@@ -136,5 +162,35 @@ module eddi
       !       r(iter+1) = lower + spacing * iter
       !    enddo
       ! end function range
+
+      ! subroutine integration_oneatom(nleb, nshell, integral)
+      !    implicit none
+      !    INTEGER, intent(in) :: nleb, nshell
+      !    INTEGER :: ileb
+      !    INTEGER :: ngrid = 0   ! number of points making up the grid
+      !    TYPE(type_grid_point), DIMENSION(:), ALLOCATABLE :: thegrid
+      !    REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: thefgrid
+      !    REAL(KIND=dp) :: integral
+
+      !    ileb = get_number_of_lebedev_grid(nleb)
+      !    ngrid = lebedev_grid(ileb)%n * nshell
+
+      !    print *, '!' // REPEAT('-', 78) // '!'
+      !    print *, '! Angular grid points: ', lebedev_grid(ileb)%n
+      !    print *, '! Radial grid points: ', nshell
+      !    print *, '! Total grid points:   ', ngrid
+      !    print *, '!' // REPEAT('-', 78) // '!'
+      !    allocate(thegrid(ngrid))
+      !    allocate(thefgrid(ngrid))
+
+      !    call build_grid(ileb, nshell, thegrid)
+      !    call evaluate_atgrid(thegrid, thefgrid)
+      !    integral = 4.0_dp*pi*sum(thefgrid)
+
+      !    deallocate(thegrid)
+      !    deallocate(thefgrid)
+
+      ! end subroutine integration_oneatom
+
 
 end module eddi
--- main.f90
+++ main.f90
@@ -1,7 +1,14 @@
 program hallo 
    USE lebedev, ONLY: dp
-   USE eddi, ONLY: integration_oneatom
+   USE eddi, ONLY: integration_twoatom
    implicit none
+   INTEGER, DIMENSION(2) :: nleb, nshell
+   REAL(KIND=dp), DIMENSION(3) :: displacement
+   REAL(KIND=dp) :: integral
 
-   call integration_oneatom(9, 5)
+   nleb = (/ 19, 19/)
+   nshell = (/50, 50/)
+   displacement = (/3.0_dp, 0.0_dp, 0.0_dp/)
+   call integration_twoatom(nleb, nshell, displacement, integral)
+   print *, integral
 end program hallo
