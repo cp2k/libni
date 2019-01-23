5c79deaea3fd0e3a3d1eb3d07d46f528ccad3cc2
Edward Ditler <editler@student.ethz.ch>
2019-01-23 09:52:43 +0100

    restructure

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-gfortran -std=f2008 -c spherical_harmonics.f90 lebedev.f90 ni_fun.f90 grid.f90 gradients.f90 eddi.f90 unit_test.f90 test_gradients.f90;
-gfortran -std=f2008 -C *.o main.f90 -o out;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -c spherical_harmonics.f90 lebedev.f90 ni_fun.f90 grid.f90 gradients.f90 eddi.f90 unit_test.f90 test_gradients.f90;
+mv *.o mods/
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o main.f90 -o out;
 if [ $? -eq 0 ]; then
     ./out
 else
