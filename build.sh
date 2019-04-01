04562b39450f3a284647134cf0fc620acd79edee
Edward Ditler <editler@student.ethz.ch>
2019-04-01 18:28:37 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-gfortran -C -o out spherical_harmonics.f90 lebedev.f90 grid.f90 gradients.f90 eddi.f90 unit_test.f90 test_gradients.f90 main.f90;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -c ni_types.f90 spherical_harmonics.f90 lebedev.f90 ni_fun.f90 ni_grid.f90 ni_gradients.f90 ni_module.f90 unit_test.f90 test_gradients.f90;
+mv *.o mods/
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o main.f90 -o out;
 if [ $? -eq 0 ]; then
     ./out
 else
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-gfortran -Wall -C -o out_test spherical_harmonics.f90 lebedev.f90 grid.f90 gradients.f90 ni_module.f90 unit_test.f90 test_gradients.f90 test_suite.f90;
+rm mods/*.o
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -c ni_types.f90 spherical_harmonics.f90 lebedev.f90 ni_fun.f90 ni_grid.f90 ni_module.f90 ni_gradients.f90 unit_test.f90 test_gradients.f90;
+mv *.o mods/
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o test_suite.f90 -o out_test;
 if [ $? -eq 0 ]; then
     ./out_test | tee log.txt
 else
