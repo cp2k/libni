6dc6dd4de22a43d7eafd4f6caa661ac69a5dff0d
Edward Ditler <editler@student.ethz.ch>
2019-04-01 18:43:26 +0200

    final cleanup

--- build.sh
+++
@@ -1,8 +0,0 @@
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -c ni_types.f90 spherical_harmonics.f90 lebedev.f90 ni_fun.f90 ni_grid.f90 ni_gradients.f90 ni_module.f90 unit_test.f90 test_gradients.f90;
-mv *.o mods/
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o main.f90 -o out;
-if [ $? -eq 0 ]; then
-    ./out
-else
-    echo 'Build failed ☹️'
-fi
