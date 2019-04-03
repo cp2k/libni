2f428088349e2e3aba11eb47369979f35a4d5cab
Edward Ditler <editler@student.ethz.ch>
2019-04-03 11:07:31 +0200

    folders

--- build.bash
+++ build.bash
@@ -1,6 +1,6 @@
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -c ni_types.f90 spherical_harmonics.f90 lebedev.f90 ni_fun.f90 ni_grid.f90 ni_gradients.f90 ni_module.f90 unit_test.f90 test_gradients.f90;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -c src/ni_types.f90 src/spherical_harmonics.f90 src/lebedev.f90 src/ni_fun.f90 src/ni_grid.f90 src/ni_gradients.f90 src/ni_module.f90 src/unit_test.f90 src/test_gradients.f90;
 mv *.o mods/
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o main.f90 -o out;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o src/main.f90 -o out;
 if [ $? -eq 0 ]; then
     ./out
 else
--- test.bash
+++ test.bash
@@ -1,7 +1,7 @@
 rm mods/*.o
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -c ni_types.f90 spherical_harmonics.f90 lebedev.f90 ni_fun.f90 ni_grid.f90 ni_module.f90 ni_gradients.f90 unit_test.f90 test_gradients.f90;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -c src/ni_types.f90 src/spherical_harmonics.f90 src/lebedev.f90 src/ni_fun.f90 src/ni_grid.f90 src/ni_module.f90 src/ni_gradients.f90 src/unit_test.f90 src/test_gradients.f90;
 mv *.o mods/
-gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o test_suite.f90 -o out_test;
+gfortran -std=f2008 -J ./mods/ -I ./mods/ -C mods/*.o src/test_suite.f90 -o out_test;
 if [ $? -eq 0 ]; then
     ./out_test | tee log.txt
 else
